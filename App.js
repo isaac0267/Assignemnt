cdec725b8f4ce453a31e4cf8d3d62fdb15494b43
Isaac Levy <isaa0267@stud.kea.dk>
2024-02-29 11:23:31 +0100

    The second assignement is ready

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+
+  async function saveList() {
+    try {
+      const jsonValue = JSON.stringify(list);
+      await AsyncStorage.setItem('@myList', jsonValue);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function pressMe() {
+    alert("" + text);
+    setList([...list, { key: list.length.toString(), value: text }]);
+    setText(""); // Optionally clear the input after adding to the list
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Assignment</Text>
+      <Text>{text}</Text>
+      <TextInput style={styles.textInput} onChangeText={setText} value={text} />
+      <Button title='Press me' onPress={pressMe} />
+      <FlatList
+        data={list}
+        renderItem={({ item }) => <Text>{item.value}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +44,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '80%',
+    marginBottom: 12,
   },
 });
