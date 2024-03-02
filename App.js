5ce369720508450cc98490d47de91e20971c9ffa
Isaac Levy <isaa0267@stud.kea.dk>
2024-03-02 20:42:37 +0100

    The new code is ready to be publish in the github

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export default function App() {
   const [text, setText] = useState("");
   const [list, setList] = useState([]);
+  saveList();
 
   async function saveList() {
     try {
